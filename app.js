3341c14c463781cb33ed096e0b87a338746f91d2
wsgouwan@126.com <wsgouwan@126.com>
2017-07-18 23:19:19 +0800

    test

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
   res.send('hello world');
 });
 
-app.listen(8090);
+app.listen(8090, '120.27.189.171');
 
 console.log('listen 8090')
-
